b553ccd970754137772dac67d9b8dd9d7e2504da
Priest <Charleschukwuemeka47@gmail.com>
2024-04-17 22:09:53 +0100

    Feat: Create a user controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Express, Request, Response } from "express";
+
+
 export default function routes(app: Express){
     //Health Check Route
     app.get("/healthcheck", (req: Request, res: Response) => {
