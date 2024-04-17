c2deae097088631573679c0aef829284b02d062c
Priest <Charleschukwuemeka47@gmail.com>
2024-04-17 22:05:22 +0100

    Feat: Add the user model, with methods

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,9 +1,20 @@
-import mongoose from "mongoose";
+import { Schema, Model } from "mongoose";
 import bcrypt from "bcrypt";
 import config from "config";
 
+
+//Type is recommended instead of Interface
+export type UserDocument = {
+    email: string;
+    name: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+    comparePassword(candidatePassword: string): Promise<boolean>
+};
+
 //Schema Definition
-const userSchema = new mongoose.Schema(
+const userSchema = new Schema <UserDocument>(
   {
     email: { type: String, required: true, unique: true },
     name: { type: String, required: true },
@@ -14,7 +25,27 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-//Model
-const UserModel = mongoose.model("User", userSchema)
+userSchema.pre("save", async function (next: () => any) {
 
-export default UserModel
+   if(!this.isModified("password")){
+    return next()
+   }
+
+   const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"))
+   const newHashedPassword = await bcrypt.hashSync(this.password, salt)
+
+   this.password = newHashedPassword;
+
+   return next();
+
+})
+
+userSchema.methods.comparePassword = async function (candidatePassword:string): Promise<boolean> {
+
+    return bcrypt.compare(candidatePassword, this.password).catch((e) => false)
+}
+
+//Model
+const UserModel = new Model("User", userSchema)
+
+export default UserModel;
